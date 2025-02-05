39b7898e90d93e2496166dffbff2b84987d33b5e
Sathish Dath <sathishdutt0@gmail.com>
2025-02-05 18:34:50 +0530

    sitemap-added

---
+++ generate-sitemap.js
@@ -0,0 +1,14 @@
+import SitemapGenerator from "sitemap-generator";
+
+// Replace with your actual website URL
+const generator = SitemapGenerator("https://rvce-utility.vercel.app/", {
+  stripQuerystring: false,
+});
+
+// Register event listeners
+generator.on("done", () => {
+  console.log("Sitemap generated!");
+});
+
+// Start the crawler
+generator.start();
