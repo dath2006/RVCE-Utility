41a8089448f028ee79674bb897cc518731fbc47a
RVCE-Utility <rvceutility@gmail.com>
2025-04-07 23:19:20 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Auth0Provider
-      domain={import.meta.env.AUTH0_DOMAIN}
-      clientId={import.meta.env.AUTH0_CLIENTID}
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENTID}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
